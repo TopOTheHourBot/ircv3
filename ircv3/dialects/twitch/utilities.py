03c598adfb58926c1685b40191a16f758711ec35
braedynl <braedynlettinga@gmail.com>
2023-10-08 17:19:26 -0400

    Add `is_ping()` utility

--- ircv3/dialects/twitch/utilities.py
+++ ircv3/dialects/twitch/utilities.py
@@ -1,12 +1,20 @@
 from __future__ import annotations
 
-__all__ = ["is_privmsg"]
+__all__ = [
+    "is_privmsg",
+    "is_ping",
+]
 
 from typing import TypeGuard
 
-from .commands import ServerPrivmsg
+from .commands import Ping, ServerPrivmsg
 
 
 def is_privmsg(value: object, /) -> TypeGuard[ServerPrivmsg]:
     """Return true if ``value`` is a ``ServerPrivmsg``, otherwise false"""
-    return type(value) is ServerPrivmsg  # Exact type check is faster for negative tests
+    return type(value) is ServerPrivmsg
+
+
+def is_ping(value: object, /) -> TypeGuard[Ping]:
+    """Return true if ``value`` is a ``Ping``, otherwise false"""
+    return type(value) is Ping
